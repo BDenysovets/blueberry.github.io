e7f28779fbbe8c144e47a74fa6efa9682868d2c9
Dan Denysovets <alen250299@gmail.com>
2019-11-12 17:03:46 +0200

    new element added

---
+++ js/sumCalc.js
@@ -0,0 +1,7 @@
+const sumCalc = () => {
+    let userValue = document.querySelector('.price-input');
+    let totalDiscount = document.querySelector('.discount-value');
+    let totalSum = document.querySelector('.sum-value');
+
+    
+}
