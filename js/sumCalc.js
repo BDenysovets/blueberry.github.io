17f6df129ffa249bab597b0a0b3fcca61e0c7486
Dan Denysovets <alen250299@gmail.com>
2019-11-25 19:14:05 +0200

    img compress

--- js/sumCalc.js
+++ js/sumCalc.js
@@ -6,19 +6,18 @@ const sumCalc = () => {
     let totalDiscount = 0;
 
     if (+userValue > 5 && +userValue <= 100) {
-        totalSum = (+userValue * 110) / 100 * 95;
+        totalSum = Math.round((+userValue * 110) / 100 * 95);
         totalDiscount = (+userValue * 110) / 100 * 5;
-        discount.innerHTML = `5 % і ${totalDiscount} грн`;
+        discount.innerHTML = `5 % - ${totalDiscount} грн`;
         sum.innerHTML = `${totalSum} грн`;
     } else if (+userValue > 100) {
-        totalSum = (+userValue * 110) / 100 * 90;
+        totalSum = Math.round((+userValue * 110) / 100 * 90);
         totalDiscount = (+userValue * 110) / 100 * 10;
-        discount.innerHTML = `10 % і ${totalDiscount} грн`;
+        discount.innerHTML = `10 % - ${totalDiscount} грн`;
         sum.innerHTML = `${totalSum} грн`;
     } else if (userValue == "" || +userValue === 0 || +userValue <= 5) {
-        totalSum = (+userValue * 120);
+        totalSum = Math.round((+userValue * 120));
         discount.innerHTML = `${totalDiscount} %`;
         sum.innerHTML = `${totalSum} грн`;
     }
-
 };
