424fa14ebd2f419611bb1f3031705f04863f9f32
Dan <bohdan.denysovets@bn.design>
2020-07-25 18:42:06 +0300

    .

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,24 +1,38 @@
 
 const menuSlide = () => {
-    const burger = document.querySelector('.burger-menu');
-    const menu = document.querySelector('.header-menu');
-    const links = document.querySelectorAll('.header-menu__links');
+    const burger = document.querySelector('.burger-menu')
+    const menu = document.querySelector('.header-menu')
+    const links = document.querySelectorAll('.header-menu__links')
 
     burger.addEventListener('click', () => {
 
-        menu.classList.toggle('header-menu-active');
+        menu.classList.toggle('header-menu-active')
 
         links.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = '';
+                link.style.animation = ''
             } else {
-                link.style.animation = `linkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+                link.style.animation = `linkFade 0.5s ease forwards ${index / 7 + 0.2}s`
             }
-        });
+        })
 
-        burger.classList.toggle('toogle');
-    });
-    
-};
+        burger.classList.toggle('toogle')
+    })
 
-menuSlide();
+    links.forEach(item => {
+        item.addEventListener("click", e => {
+            menu.classList.remove('header-menu-active')
+            burger.classList.remove('toogle')
+
+            links.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ''
+                } else {
+                    link.style.animation = `linkFade 0.5s ease forwards ${index / 7 + 0.2}s`
+                }
+            })
+        })
+    })
+}
+
+menuSlide()
